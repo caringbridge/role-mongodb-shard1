ebb4223902884a458e3ea3cf162c4980f4d6121c
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2014-12-09 12:34:31 -0600

    first commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'role-mongodb-shard1'
+maintainer       'Stephen Van Zee'
+maintainer_email 'svanzee@caringbridge.org'
+license          'All rights reserved'
+description      'Installs/Configures role-mongodb-shard1'
+long_description 'Installs/Configures role-mongodb-shard1'
+version          '0.1.0'
+
+depends 'mongodb', '0.16.1'
+depends 'yum'
+
