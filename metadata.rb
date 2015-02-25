cb9c74baba75ce7e8f08a00c3bf58f557186d4bb
svanzee4cb <svanzee@Van-Zees-MacBook-Pro.local>
2015-02-25 10:53:25 -0600

    removed setting instance_name it was causing issues

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email 'svanzee@caringbridge.org'
 license          'All rights reserved'
 description      'Installs/Configures role-mongodb-shard1'
 long_description 'Installs/Configures role-mongodb-shard1'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'mongodb', '0.16.1'
 depends 'yum'
-
